d3bfed7cc57be111e51908f48cd6e90b2f85b101
Lucas Henderson <lucasjhenderson@gmail.com>
2015-10-13 17:55:56 -0600

    Added blank line after shebang and changed var 'n' to 'number'
    [#104976002]

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
-1.upto(100) do |n|
-  if n % 3 == 0 && n % 5 == 0
+
+1.upto(100) do |number|
+  if number % 3 == 0 && number % 5 == 0
     puts 'FizzBuzz'
-  elsif n % 3 == 0
+  elsif number % 3 == 0
     puts 'Fizz'
-  elsif n % 5 == 0
+  elsif number % 5 == 0
     puts 'Buzz'
   else
-    puts n
+    puts number
   end
 end
