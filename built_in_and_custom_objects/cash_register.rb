641b99e31a240604ef90654c6b204268519110d2
Lucas Henderson <lucasjhenderson@gmail.com>
2015-10-16 02:23:38 -0600

    Created cash_register.rb, tested for required output
    [#105361702]

---
+++ built_in_and_custom_objects/cash_register.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+class CashRegister
+  attr_reader :total
+
+  def initialize
+    @total = 0.0
+    puts ''
+    puts 'Your total is $0.00'
+    puts ''
+    puts 'Actions:'
+    puts "'register.purchase(float_number_here)' for a purchase."
+    puts "'register.total' to check the current total."
+    puts "'register.pay' to pay balance."
+    puts''
+  end
+
+  def purchase(item_value)
+    @total += item_value
+  end
+
+  def pay(amount)
+    @total -= amount
+    if @total > 0
+      puts "Your new total is $#{@total}"
+    elsif @total == 0
+      puts "Thank you, come again!"
+      @total = 0.0
+    else
+      @total < 0
+      puts "Your change is $#{@total}"
+      @total = 0.0
+    end
+  end
+end
+
+puts "Welcome to things'r'us. Type 'register = CashRegister.new' to begin the transaction."
