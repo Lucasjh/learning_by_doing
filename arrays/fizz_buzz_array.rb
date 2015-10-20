2c194753a8cc261c39988525605129c006f812f7
Lucas Henderson <lucasjhenderson@gmail.com>
2015-10-19 18:36:28 -0600

    Edited fizz_buzz_array.rb, checked with with rubocop.
    [#105166156]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,21 +1,18 @@
 #!/usr/bin/env ruby
 
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+array_to_100 = (1..100).to_a
 
-numbers = [one_to_onehundred]
-
-def one_to_onehundred
- 1.upto(100) do |n|
-    if n % 3 == 0 && n % 5 == 0
-      puts 'FizzBuzz'
-    elsif n % 3 == 0
-      puts 'Fizz'
-    elsif n % 5 == 0
-      puts 'Buzz'
-    else
-      puts n
-    end
- end
+def fizz_buzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    'FizzBuzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number
+  end
 end
 
-puts numbers
+fizz_buzzed = array_to_100.map {|number| fizz_buzz(number)}
+puts fizz_buzzed
