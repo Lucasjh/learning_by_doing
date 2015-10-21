b3029127d29a7a93994d587b5d36799df0a29dcf
Lucas Henderson <lucasjhenderson@gmail.com>
2015-10-21 11:07:34 -0600

    Checked with homework tester, edited for corrections
    [#105165762]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-deca = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-deca_minus = deca - [5, 6, 7]
-deca_unshift = (deca - [5, 6, 7]).unshift(5)
-deca_push = (deca - [5, 6, 7]).unshift(5).push(6)
+deca = [1,2,3,4,5,6,7,8,9,10]
+deca_minus = '[' + (deca - [5, 6, 7]).join(',') + ']'
+deca_unshift = '[' + (deca - [5, 6, 7]).unshift(5).join(',') + ']'
+deca_push = '[' + (deca - [5, 6, 7]).unshift(5).push(6).join(',') + ']'
 deca_reject = deca.reject { |a| a < 9 }
 empty_a = (deca - deca).length
 
@@ -14,10 +14,10 @@ puts "The first element is #{deca.first}"
 puts "The third element is #{deca[2]}"
 puts "The element with an index of 3 is #{deca[3]}"
 puts "The second from last element is #{deca[8]}"
-puts 'The first four elements are ' + "'" + deca.take(4) * ',' + "'"
-puts "If we delete 5, 6, and 7 from the array, we're left with #{deca_minus}"
+puts 'The first four elements are ' + "'" + deca.take(4) * ', ' + "'"
+puts "If we delete 5, 6 and 7 from the array, we're left with #{deca_minus}"
 puts "If we add 5 at the beginning of the array,\
  we're left with #{deca_unshift}"
 puts "If we add 6 at the end of the array, we're left with #{deca_push}"
-puts "Only the elements #{deca_reject} are > 8"
+puts "Only the elements #{deca_reject} are > 8."
 puts "If we remove all the elements, then the length of the array is #{empty_a}"
